041aa7099c4860f698b9b310fb12fbd4c4009fd8
Anonymous1231234534235423423 <143645138+Ducklord42@users.noreply.github.com>
2024-03-07 09:10:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -31,38 +31,17 @@ function savef()
 	URL.revokeObjectURL(link.href);
 }
 
-function loadf()
-{
-	let input = document.querySelector("input[type='file']");
-	const reader = new FileReader();
-	reader.onload = function ()
-	{
-		dta = JSON.parse(reader.result);
-    	for (i = 0; i != 2; i++)
-    	{
-    		document.getElementById("p" + String(i)).innerHTML = dta[i];
-    	}
-	}
-	let ff = new Blob([], {lastModified: 1709563128000, name: "gallery.json", size: 153, type: "application/json", webkitRelativePath: ""});
-	reader.readAsText(input.files[0]);
-	//reader.readAsText(ff);
-}
-
 var url = 'https://ducklord42.github.io/postapost/gallery.json';
 
 fetch(url)
   .then(function(response) {
     response.text().then(function(text) {
       dta = JSON.parse(text);
-      done();
-    });
-  });
-
-function done() {
-  for (i = 0; i != 2; i++)
+      for (i = 0; i != 2; i++)
 	{
     		document.getElementById("p" + String(i)).innerHTML = dta[i];
     	}
-}
+    });
+  });
 
 console.log("This works");
